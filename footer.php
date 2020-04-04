9354a62d5ad815840d5cd82002b7354344ebaed4
Faber Vergara <fabervergara@gmail.com>
2020-04-04 09:19:42 -0500

    add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,43 @@
+      <footer class="py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="" class="img-fluid">
+              <p class="mt-2">
+                <a href="#" class="text-blue"><i class="fab fa-facebook fa-2x mr-2"></i></a>
+                <a href="#" class="text-blue"><i class="fab fa-instagram fa-2x"></i></a>
+              </p>
+              <p class="mt-5 text-white">
+                <a href="#" class="text-white">Términos y condiciones</a> | <a href="#" class="text-white">Política de privacidad</a>
+              </p>
+            </div>
+            <div class="col-md-4">
+              <div class="media">
+                <i class="fas fa-map-marker-alt fa-2x text-blue mr-3"></i>
+                <div class="media-body text-white">
+                  <p class="mt-0">Edificio CITIMED Av. Mariana de Jesús y Nuño Valderrama. Piso 5, consultorio 552
+                  Quito, Ecuador</p>
+                </div>
+              </div>
+              <div class="media">
+                <i class="fas fa-phone fa-2x text-blue mr-3"></i>
+                <div class="media-body text-white">
+                  <h5 class="text-rose mb-0"><strong>1800 ADELGAZA</strong></h5>
+                  <h5 class="ml-5 pl-2"><strong>2 335 4292</strong></h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <p class="text-white"><strong>SUSCRÍBETE A NUESTRO BOLETÍN </strong></p>
+              <input type="email" name="" class="form-control" placeholder="Email">
+              <button class="btn btn-primary mt-2 px-4 rounded-pill">SUSCRIBIRME</button>
+              <p class="mt-5 text-white">
+                Copyright <i class="far fa-copyright"></i> 2020 | Development by <a href="#" class="text-white"><strong>Gestión</strong></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
     <?php // include dirname(__FILE__) . '/helpers/wave-svg.html' ?>
     <?php wp_footer(); ?>
