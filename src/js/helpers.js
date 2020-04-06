38fb3cacb3283c8f674a5b4f745b4dcf0c2a79e5
Faber Vergara <fabervergara@gmail.com>
2020-04-05 22:07:10 -0500

    clean helpers file

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,8 +1,2 @@
 jQuery(document).ready(function ($) {
-  $('#main-carousel-prev').on('click', function () {
-    $('#main-carousel').carousel('prev')
-  })
-  $('#main-carousel-next').on('click', function () {
-    $('#main-carousel').carousel('next')
-  })
 })
