06f65d2389a19ab58eb67059c1b3a0b264710280
Faber Vergara <fabervergara@gmail.com>
2020-04-07 20:00:13 -0500

    fix map marker

--- src/js/map.js
+++ src/js/map.js
@@ -3,5 +3,17 @@ jQuery(document).ready(function ($) {
     map = L.map('map', { scrollWheelZoom: false }).setView(cordinates, 15)
 
   L.tileLayer.provider('CartoDB.Voyager').addTo(map)
-  L.marker(cordinates).addTo(map)
+
+  let customIcon = L.icon({
+    iconUrl: themePath + '/img/marker.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize: [35, 50], // size of the icon
+    // shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+  L.marker(cordinates, {icon: customIcon}).addTo(map)
 })
