9ab73ef811ad1c5b8bb229fd8b57a754e528d6bd
Faber Vergara <fabervergara@gmail.com>
2020-04-13 15:48:08 -0500

    fix carousel caption layout

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -16,7 +16,7 @@ Vue.component('customCarousel', {
       })
   },
   template: `
-    <div id="main-carousel" class="carousel slide" data-ride="carousel">
+    <div id="main-carousel" class="carousel slide wave-carousel-blue" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item" :class="{ 'active': key === 0 }" v-for="(slide, key) in sliderArray">
           <img :src="slide.post_image" class="d-block w-100" alt="...">
@@ -27,9 +27,13 @@ Vue.component('customCarousel', {
           </div>
         </div>
       </div>
-      <div class="carousel-controls-container wave wave-blue py-4 pl-4">
-        <button class="btn btn-secondary text-white" id="main-carousel-prev"><img class="img-fluid" :src="themePath + '/img/angle-left.png'"></button>
-        <button class="btn btn-secondary text-white" id="main-carousel-next"><img class="img-fluid" :src="themePath + '/img/angle-right.png'"></button>
-      </div>
+      <a class="carousel-control-prev" href="#main-carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#main-carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>`
 })
