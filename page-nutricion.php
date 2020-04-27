98675db790ee29bc5212e0105c332b7295a69765
Faber Vergara <fabervergara@gmail.com>
2020-04-26 20:40:48 -0500

    add page-main-image section

--- page-nutricion.php
+++ page-nutricion.php
@@ -1,8 +1,21 @@
 <?php get_header(); ?>
-  <section>
+  <section class="wave-dark-bottom">
     <div class="row">
-      <div class="col">
-        <custom-carousel slider="nutricion" color="rose"></custom-carousel>
+      <div class="col page-main-image" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id($post_id) ) ?>)">
+        <div class="side-container">
+          <h2 class="subtitle">
+            Baja de peso <span class="caveat text-secondary">ahora</span> sin cirugía Balón intragástrico
+          </h2>
+          <ul class="mt-4">
+            <li>Un procedimiento eficaz, indoloro y mínimamente invasivo</li>
+            <li>Permite bajar de peso rápido y de forma segura</li>
+            <li>En apenas 6 meses, puedes llegar a perder hasta 20 kg</li>
+          </ul>
+          <div class="contact-form-container p-4 mt-4">
+            <h3 class="text-center form-title text-blue mb-4">¡Te llamaremos!</h3>
+            <?php echo do_shortcode('[contact-form-7 id="6" title="Principal"]'); ?>
+          </div>
+        </div>
       </div>
     </div>
   </section>
