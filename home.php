fca7aae8c226fe7a7f9d8c9437df2665ed0fe1da
Faber Vergara <fabervergara@gmail.com>
2020-05-03 22:46:09 -0500

    fix dat format

--- home.php
+++ home.php
@@ -6,10 +6,14 @@
       <div class="col-md-8">
         <?php if (have_posts()) :
           while (have_posts()) : the_post(); ?>
-            <div class="wave wave-white wave-white-bottom">
-              <?php the_post_thumbnail('large', array('class' => 'img-fluid')) ?>
-            </div>
-            <small class="text-muted">Por <?php the_author() ?> | <?php the_time('F jS, Y') ?> | en <?php the_category(', ') ?> | <i class="far fa-comment"></i> <?php comments_popup_link('0 Comentarios', '1 Comentario', '% Comentarios'); ?></small>
+            <?php if ( has_post_thumbnail() ): ?>
+              <div class="wave wave-white wave-white-bottom">
+                <?php the_post_thumbnail('large', array('class' => 'img-fluid')) ?>
+              </div>
+            <?php endif ?>
+            <small class="text-muted">
+              Por <?php the_author() ?> | <?php echo date_i18n('F j, Y') ?> | en <?php the_category(', ') ?> | <i class="far fa-comment"></i> <?php comments_popup_link('0 Comentarios', '1 Comentario', '% Comentarios'); ?>
+            </small>
             <h5><strong><?php the_title(); ?></strong></h5>
             <div class="entry mb-5">
               <?php the_excerpt(); ?>
--- single.php
+++ single.php
@@ -6,9 +6,11 @@
       <div class="col-md-8">
         <?php if (have_posts()) :
           while (have_posts()) : the_post(); ?>
-            <div class="wave wave-white wave-white-bottom">
-              <?php the_post_thumbnail('large', array('class' => 'img-fluid')) ?>
-            </div>
+            <?php if ( has_post_thumbnail() ): ?>
+              <div class="wave wave-white wave-white-bottom">
+                <?php the_post_thumbnail('large', array('class' => 'img-fluid')) ?>
+              </div>
+            <?php endif ?>
             <small class="text-muted">Por <?php the_author() ?> | <?php the_time('F jS, Y') ?> | en <?php the_category(', ') ?> | <i class="far fa-comment"></i> <?php comments_popup_link('0 Comentarios', '1 Comentario', '% Comentarios'); ?></small>
             <h3><strong><?php the_title(); ?></strong></h3>
             <div class="entry mt-4">
