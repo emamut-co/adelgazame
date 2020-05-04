51ee9c4ab0601cdb5bd65b7f8367f2e06634fbc4
Faber Vergara <fabervergara@gmail.com>
2020-05-04 04:53:06 -0500

    fix blog and single layout

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
               Por <?php the_author() ?> | <?php echo date_i18n('F j, Y') ?> | en <?php the_category(', ') ?> | <i class="far fa-comment"></i> <?php comments_popup_link('0 Comentarios', '1 Comentario', '% Comentarios'); ?>
             </small>
             <h5><strong><?php the_title(); ?></strong></h5>
-            <div class="entry mb-5">
+            <div class="entry blog mb-5">
               <?php the_excerpt(); ?>
               <a href="<?php echo get_post_permalink() ?>" class="btn btn-secondary">Leer más</a>
             </div>
