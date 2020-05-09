99263002bb6fb9f411e7f980d62c0a82159de03e
Faber Vergara <fabervergara@gmail.com>
2020-05-09 06:54:12 -0500

    fix footer layout

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
       <footer class="py-5">
         <div class="container">
-          <div class="row mt-5">
+          <div class="row mt-5 d-none d-md-flex">
             <div class="col-md-4">
               <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="" class="img-fluid">
               <p class="mt-2">
@@ -32,6 +32,35 @@
               <?php echo do_shortcode('[contact-form-7 id="113" title="Suscribe"]') ?>
               <p class="mt-5 text-white">
                 Copyright <i class="far fa-copyright"></i> 2020 | Development by <a href="#" class="text-white"><strong>Gestión</strong></a>
+              </p>
+            </div>
+          </div>
+          <div class="row mt-5 d-flex d-md-none">
+            <div class="col-12 text-center">
+              <p class="mt-2">
+                <a href="<?php echo get_option('facebook') ?>" class="text-blue"><i class="fab fa-facebook fa-3x mr-2"></i></a>
+                <a href="<?php echo get_option('instagram') ?>" class="text-blue"><i class="fab fa-instagram fa-3x"></i></a>
+              </p>
+              <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-12 text-center mt-5">
+              <i class="fas fa-map-marker-alt fa-2x text-blue mr-3"></i>
+              <p class="mt-3 text-white">Edificio CITIMED Av. Mariana de Jesús y Nuño Valderrama. Piso 5, consultorio 552
+              Quito, Ecuador</p>
+            </div>
+            <div class="col-12 text-center mt-5">
+              <i class="fas fa-phone fa-2x text-blue mr-3"></i>
+              <h5 class="text-rose mb-0"><strong>1800 ADELGAZA</strong></h5>
+              <h5 class="text-white ml-5 pl-2"><strong>2 335 4292</strong></h5>
+            </div>
+            <div class="col-12 text-center mt-5">
+              <p class="text-white"><strong>SUSCRÍBETE A NUESTRO BOLETÍN </strong></p>
+              <?php echo do_shortcode('[contact-form-7 id="113" title="Suscribe"]') ?>
+              <p class="mt-3 text-white">
+                <a href="#" class="text-white">Términos y condiciones</a> <br> <a href="#" class="text-white">Política de privacidad</a>
+              </p>
+              <p class="mt-2 text-white">
+                Copyright <i class="far fa-copyright"></i> 2020 <br> Development by <a href="#" class="text-white"><strong>Gestión</strong></a>
               </p>
             </div>
           </div>
